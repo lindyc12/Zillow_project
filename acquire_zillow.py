23410a0a46b7d45cc1c1c21149a3dd6e906265ab
Lindy Castellaw <lindy.c.castellaw@gmail.com>
2022-08-05 09:17:04 -0500

    acquire

---
+++ acquire_zillow.py
@@ -0,0 +1,43 @@
+from env import host, user, password
+import env
+import pandas as pd
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    
+sql_query = '''
+SELECT prop.*,
+        predictions_2017.logerror,
+        predictions_2017.transactiondate,
+        air.airconditioningdesc,
+        arch.architecturalstyledesc,
+        build.buildingclassdesc,
+        heat.heatingorsystemdesc,
+        land.propertylandusedesc,
+        story.storydesc,
+        type.typeconstructiondesc
+        FROM properties_2017 prop
+        JOIN (
+            SELECT parcelid, MAX(transactiondate) AS max_transactiondate
+            FROM predictions_2017
+            GROUP BY parcelid
+            ) pred USING(parcelid)
+        JOIN predictions_2017 ON pred.parcelid = predictions_2017.parcelid
+                          AND pred.max_transactiondate = predictions_2017.transactiondate
+        LEFT JOIN airconditioningtype air USING(airconditioningtypeid)
+        LEFT JOIN architecturalstyletype arch USING(architecturalstyletypeid)
+        LEFT JOIN buildingclasstype build USING(buildingclasstypeid)
+        LEFT JOIN heatingorsystemtype heat USING(heatingorsystemtypeid)
+        LEFT JOIN propertylandusetype land USING(propertylandusetypeid)
+        LEFT JOIN storytype story USING(storytypeid)
+        LEFT JOIN typeconstructiontype type USING(typeconstructiontypeid)
+        WHERE propertylandusedesc = "Single Family Residential"
+            AND transactiondate <= '2017-12-31'
+            AND prop.longitude IS NOT NULL
+            AND prop.latitude IS NOT NULL
+'''
+    
+def get_zillow_data():
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+    df = df.drop(columns='id')
+    return df
